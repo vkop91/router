7ea714729e08dbc752753f275fe33c423a31d702
vk <testfakemail@yandex.ru>
2018-06-20 17:13:13 +0300

    dev clear

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 // Иницилизация основных классов
 // Обработчик ошибок
 require_once __DIR__ . '/../core/Logs.php';
